88f51b8ce0fc74ac8eec226f2a12eb2ee5100b2a
Luis Hankel <kaes3kuch3n@gmail.com>
2018-05-26 12:58:29 +0200

    Background color changer

--- iOS-Test/ViewController.swift
+++ iOS-Test/ViewController.swift
@@ -19,7 +19,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func changeColor(sender: UIButton) {
+        let r = CGFloat(arc4random() % 256);
+        let g = CGFloat(arc4random() % 256);
+        let b = CGFloat(arc4random() % 256);
+        
+        let color = UIColor(red: r / 255.0, green: g / 255.0, blue: b / 255.0, alpha: 1.0);
+        
+        view.backgroundColor = color;
+    }
+    
 }
 
